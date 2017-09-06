19f1183dfdb033c0047edbf08477db57c89bb9a3
Ömer Mindivanli <omer.mindivanli@gmail.com>
2017-09-06 19:12:19 +0300

    Update The func of  ExtractJwt.fromAuthHeader();
    Last Passport.js does not support this func. I fixed that like this ;
      opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const config = require('../config/database');
 
 module.exports = function(passport){
   let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
   opts.secretOrKey = config.secret;
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     User.getUserById(jwt_payload._doc._id, (err, user) => {
