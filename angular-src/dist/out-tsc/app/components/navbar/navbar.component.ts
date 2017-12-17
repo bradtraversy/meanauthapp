328d13c68dd677b508adf49d43fe2a94f88415f7
Tony <tomcatbuzz@yahoo.com>
2017-12-17 01:54:30 -0500

    Add files via upload

---
+++ angular-src/dist/out-tsc/app/components/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService) { }
+
+  ngOnInit() {
+  }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.show('You are logged out', {
+      cssClass: 'alert-success', timeout: 3000
+    });
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
---
+++ angular-src/dist/out-tsc/app/services/auth.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
+
+@Injectable()
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private http: Http) {
+      this.isDev = true;  // Change to false before deployment
+      }
+
+  registerUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('users/register', user, {headers: headers})
+      .map(res => res.json());
+  }
+
+  authenticateUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('users/authenticate', user, {headers: headers})
+      .map(res => res.json());
+  }
+
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('users/profile', {headers: headers})
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired('id_token');
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
+}
