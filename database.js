df197f29e92caaf958a361ef507c664d0b19c826
Tony <tomcatbuzz@yahoo.com>
2017-12-17 01:19:23 -0500

    Delete database.js

--- database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  database: 'mongodb://tomcatbuzz:buzzinator@ds141786.mlab.com:41786/meanauthapp',   //prod
-  //database: 'mongodb://localhost:27017/meanauth',    //dev
-  secret: 'yoursecret'
-}
