d205be6b8fd7b7cbbbabcaf5199a42c935d2c51a
Tony <tomcatbuzz@yahoo.com>
2018-01-14 23:46:36 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-  database: 'mongodb://tomcatbuzz:buzzinator@ds141786.mlab.com:41786/meanauthapp',   //prod
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
